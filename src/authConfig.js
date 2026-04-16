c037eaf434ad402e46871a89000c2a763318d29f
Anushka <dhawasanushka9@gmail.com>
2026-04-16 22:21:52 +0530

    fix auth config with redirect uri

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,8 +1,26 @@
 export const authConfig = {
   clientId: "fitness-app",
-  authorizationEndpoint: "https://keycloak-deploy-st9e.onrender.com/realms/fitness-oauth2/protocol/openid-connect/auth",
-  tokenEndpoint: "https://keycloak-deploy-st9e.onrender.com/realms/fitness-oauth2/protocol/openid-connect/token",
-  redirectUri: import.meta.env.VITE_REDIRECT_URI,   
+
+  authorizationEndpoint:
+    "https://keycloak-deploy-st9e.onrender.com/realms/fitness-oauth2/protocol/openid-connect/auth",
+
+  tokenEndpoint:
+    "https://keycloak-deploy-st9e.onrender.com/realms/fitness-oauth2/protocol/openid-connect/token",
+
+  redirectUri: import.meta.env.VITE_REDIRECT_URI,
+
   scope: "openid profile email",
+
+  
+  getAuthUrl: (codeChallenge) => {
+    return `${authConfig.authorizationEndpoint}?response_type=code` +
+      `&client_id=${authConfig.clientId}` +
+      `&redirect_uri=${encodeURIComponent(authConfig.redirectUri)}` +
+      `&scope=${encodeURIComponent(authConfig.scope)}` +
+      `&code_challenge=${codeChallenge}` +
+      `&code_challenge_method=S256`;
+  },
+
+  
   onRefreshTokenExpire: (event) => event.logIn(),
-}
+};
